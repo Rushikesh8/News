b67452c02d1c09805f1591ea08f91beb77548174
Rushikesh8 <rushikeshsakharwade@gmail.com>
2023-04-11 23:29:01 +0530

    created model

--- News/settings.py
+++ News/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e-1hksb=y@5+$*-7jjzlkmxw!pd2%14y6noy^s6vvtqezotai=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'news_app'
 ]
 
 MIDDLEWARE = [
--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+def upload_image(instance, filename):
+    return f'images/{filename}'
+
+class News(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=None)
+    author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=upload_image, blank=True, null=True)
+    source = models.CharField(max_length=100)
